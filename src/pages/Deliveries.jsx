796beb5a47de28d5b5775e86a99c7de2b45fdadb
Shafiq Shekh <sheikhrafiullah57@gmail.com>
2026-09-05 19:36:56 +0530

    Deliveries page ready without fixing gap

--- src/pages/Deliveries.jsx
+++ src/pages/Deliveries.jsx
@@ -1,6 +1,10 @@
 import AllDerliveries from "../components/AllDeliveries";
+import DeliveriesTrend from "../components/DeliveriesTrend";
+import DeliveryChart from "../components/DeliveryChart";
 import DeliveryTracking from "../components/DeliveryTracking";
+import DonutChart from "../components/DonutChart";
 import StateCard from "../components/StateCard 2";
+import TopDelivery from "../components/TopDelivery";
 
 function Deliveries() {
   return (
@@ -10,6 +14,11 @@ function Deliveries() {
         <div className="flex justify-between">
           <AllDerliveries></AllDerliveries>
           <DeliveryTracking></DeliveryTracking>
+        </div>
+        <div className="flex items-center gap-2 mb-30">
+          <DonutChart></DonutChart>
+          <DeliveriesTrend></DeliveriesTrend>
+          <TopDelivery></TopDelivery>
         </div>
       </div>
     </>
